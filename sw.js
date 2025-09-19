777abeab8a6dcb87ac84a215d6d3fc17dcd0bedf
Voci-Teacher <sashtowa@yahoo.de>
2025-09-19 13:51:52 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'vocab-quiz-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data.js',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
