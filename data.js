fb502dc5436a8042122ed3fe48059342352bbb61
Voci-Teacher <sashtowa@yahoo.de>
2025-09-19 13:51:16 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+window.VOCAB_DATA = [
+  ["une histoire","eine Geschichte","C'est une histoire amusante."],
+  ["il y a","es hat, es gibt","Il y a un accident."],
+  ["un accident","ein Unfall","La cloche sonne."],
+  ["la cloche","die Glocke","La cloche sonne."],
+  ["sonner","klingeln, läuten","La cloche sonne."],
+  // … (alle weiteren Einträge aus deiner Excel hier einsetzen)
+];
